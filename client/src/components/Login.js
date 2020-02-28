ab75de16ded54b774d4e74c5b347e941c54d02a8
Alison Law <alisparklaw@gmail.com>
2020-02-28 08:19:55 +0000

    Login jsx rendering

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useRef }  from "react";
 import axios from "axios";
 import {axiosWithAuth} from '../auth/axiosWithAuth';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const usernameRef = useRef();
+  const passwordRef = useRef();
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+        <h1>Welcome to the Bubble App!</h1><br/>
+      <div className="login">
+      <div className="login-inputs">
+        username <input ref={usernameRef} type="text" />
+        <br />
+        password <input ref={passwordRef} type="text" />
+      </div>
+
+      <div>
+        <button >Submit</button>
+      </div>
+    </div>
+    
     </>
   );
 };
