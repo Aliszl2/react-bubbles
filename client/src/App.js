d0408cea2c2ed29dd5790d77f21b483d87c50bf2
Alison Law <alisparklaw@gmail.com>
2020-02-28 08:49:23 +0000

    Login working and returning a token

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,36 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  withRouter
+} from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem("token") ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/" />
+      )
+    }
+  />
+);
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+
+        <PrivateRoute path="/BubblePage" component={BubblePage} />
       </div>
     </Router>
   );
 }
-
-export default App;
+export default withRouter(App);
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,34 @@
 import React, { useRef }  from "react";
 import axios from "axios";
-import {axiosWithAuth} from '../auth/axiosWithAuth';
+// import {axiosWithAuth} from '../auth/axiosWithAuth';
+// import {useHistory} from "react-router-dom"
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-
+const Login = (props) => {
+  console.log(props);
+  // const history =useHistory();
   const usernameRef = useRef();
   const passwordRef = useRef();
+  // make a post request to retrieve a token from the api
+  // when you have handled the token, navigate to the BubblePage route
+  const submit = () => {
+    
+    axios
+      .post("http://localhost:5000/api/login", {
+        username: usernameRef.current.value,
+        password: passwordRef.current.value
+      })
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.payload);
+           props.history.push("/BubblePage");
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  };
   return (
     <>
-        <h1>Welcome to the Bubble App!</h1><br/>
+      <h1>Welcome to the Bubble App!</h1><br/>
       <div className="login">
       <div className="login-inputs">
         username <input ref={usernameRef} type="text" />
@@ -19,12 +37,12 @@ const Login = () => {
       </div>
 
       <div>
-        <button >Submit</button>
+        <button onClick={submit}>Submit</button>
       </div>
     </div>
-    
     </>
   );
 };
 
 export default Login;
+
